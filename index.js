180c516fb35316e62136ec951d358ba9be81cdcb
Eric Rodriguez <emorque10@gmail.com>
2024-11-30 22:26:32 -0500

    Added translation functions and error handling

--- index.js
+++ index.js
@@ -27,31 +27,103 @@
 //       // The translator can't be used at all.
 //   }
 
-// Button for translating
-button = document.getElementById("entry_submission");
+// Section for the starting page (text entry)
 
-// Grabbing entry and result textboxes
-inputText = document.getElementById("entry");
-resultText = document.getElementById("result");
+// Button for text submission
+const button = document.getElementById("entry_submission");
 
-// Getting Language A and B
-// const formA = document.getElementById("formA");
-// const formB = document.getElementById("formB"); 
+// Entry textbox
+const inputText = document.getElementById("entry");
+const resultText = document.getElementById("result");
 
-async function getTranslator() {
+// Section for text entry 
+const entryPage = document.getElementById("entryPage");
+
+// Getting the character limit
+const charCount = document.getElementById("char_limit");
+
+// const sentencesPage = document.getElementById("sentencesPage");
+
+// English punctuation marks:
+const punctuation = {
+    "en": "!?.",
+    "es": ".¿?¡!",
+    "ja": "。！？!?."
+};
+
+// Event Listener to update the char count element so that the user can track their character count
+// 
+inputText.addEventListener("input", () => {
+    charCount.textContent = `${inputText.textContent.length}/1000`
+    if (inputText.textContent.length > 1000) {
+        charCount.style.color = "red";
+    }
+    else {
+        charCount.style.color = "black";
+    }
+})
+
+
+async function translateText(translator) {
+    console.log(inputText.textContent);
+    resultText.textContent = await translator.translate(inputText.textContent);
+    // console.log(await translator.translate(inputText.textContent));
+}
+
+// function to obtain the languages from the forms 
+function getLanguages(form) {
+    const selected = document.querySelector(`input[name="${form}"]:checked`);
+    // If a language is selected, reutrn it 
+    if (selected){
+        return selected.value;;
+    }
+    // Else, return "null"
+    else {
+        return "null";
+    }
+}
+
+function verifyEntry() {
+    // Calling to get the languages selected
     const languageA = getLanguages("languageA");
     const languageB = getLanguages("languageB");
-    console.log(languageA, languageB);
+    // console.log(languageA, languageB);
 
+    // if either language is unselected, alert user
     if (languageA === "null" || languageB === "null") {
         alert("select a language");
         return;
     }
 
-    if (languageA === languageB) {
-        alert("same language selected");
+    // Currently, the translation API is said to only support bi-directional translations bettwen English-Spanish and English-Japanese
+    // https://docs.google.com/document/d/1bzpeKk4k26KfjtR-_d9OuXLMpJdRMiLZAOVNMuFIejk/edit?tab=t.0
+    // Will remove/edit this as support changes
+    if ((languageA === "es" && languageB === "ja") || (languageA === "ja" && languageB === "es")) {
+        alert("Bi-directional Translations Between Spanish and Japanese are not yet Supported");
         return;
     }
+
+    // if both languages are the same, alert user 
+    if (languageA === languageB) {
+        alert("Please Select Two Different Languages");
+        return;
+    }
+
+    // If the entry textbox is empty, alert use
+    if (inputText.textContent === "") {
+        alert("Plase Enter Text");
+        return;
+    }
+
+
+
+    // If everything is valid, start the game
+    gameStart(languageA, languageB, inputText.textContent)
+}
+
+async function gameStart(languageA, languageB, entryText){
+    console.log(languageA, languageB);
+    let score = 0;
 
     const languagePair = {
         sourceLanguage: languageA, // Or detect the source language with the Language Detection API
@@ -68,27 +140,62 @@ async function getTranslator() {
     }
     else {
         console.log("Translator cannot be used");
+        return;
     }
-    translateText(translator);
+
+    // By default, the entry text will be spliced by punctuation
+    // At first, I thought of using split with multiple parameters, but I would end up losing the punctuation marks I was using to split
+    // Doing it manually would be a easier to maintain the entire sentence as a whole, 
+    // and would allow me to make unique parameters for each language. For example, "upside down ?" for spanish
+
+    // List of sentences to then be iterated through
+    let sentences = [];
+    let currentSentence = []; // Each sentence will be built with a list, then turned to string with .join 
+    for (let char of entryText) {
+        // If a punctuation mark is found, add it to the built sentence and add it as a string to the sentences list
+        if (punctuation[languageA].includes(char)) {
+            if (currentSentence.length === 0){
+                continue;
+            }
+            currentSentence.push(char);
+            sentences.push(currentSentence.join("")); // According to some sources, this is vastly more optimal than continuous "+" 
+            currentSentence = []; // Reset the currentSentence list to build the next string
+        }
+        // If there is empty space that is not apart of a sentence, ignore it 
+        else if ((char === ' ' || char.charCodeAt(0) === 160) && currentSentence.length === 0) { // " " seems to have the code of 160, so its included here
+            continue;
+        }
+        else {
+            currentSentence.push(char);
+        }
+    }
+
+    // Catches if a sentence is built without a punctation mark at the end. Ex: "hello"
+    if (sentences.len !== 0) {
+        currentSentence.push(".");
+        sentences.push(currentSentence.join("")); // According to some sources, this is vastly more optimal than continuous "+" 
+    } 
+    
+    for (let sentence of sentences) {
+        setTimeout(async () => {
+            // translateText(sentence);
+            console.log(sentence);
+            resultText.textContent = await translator.translate(sentence);
+        }, 2000);
+    }
+    
+    // I'll add a way for users to manually enter sentences themselves
 }
 
-async function translateText(translator) {
-    console.log(inputText.textContent);
-    resultText.textContent = await translator.translate(inputText.textContent);
-}
+// button.addEventListener("click", getTranslator);
+button.addEventListener("click", verifyEntry);
+// button.addEventListener("click", testingJS);
 
-button.addEventListener("click", getTranslator);
-
-function getLanguages(form) {
-    const selected = document.querySelector(`input[name="${form}"]:checked`);
-    if (selected){
-        const language = selected.value;
-        // console.log(language);
-        return language;
+function testingJS() {
+    if (english.includes("!")) {
+        console.log("True");
     }
-    else {
-        // console.log("null");
-        return "null";
+    else{
+        console.log("False");
     }
 }
-console.log(formA);
